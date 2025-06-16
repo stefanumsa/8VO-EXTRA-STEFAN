25f39053fe09fe690c14da5c2e67fae65f36be1b
stefan12345oe@gmail.com <stefan12345oe@gmail.com>
2025-06-16 17:29:39 -0600

    16/06/2025

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { View, Text } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ marginBottom: 20 }}>Pantalla Principal</Text>
+      <Text style={{ marginBottom: 20 }}>Esta Aplicacion fue Creada por Stefan Oebels Sanchez.</Text>
+      <Text style={{ marginBottom: 20 }}>Certificacion de Software - Extraordinario.</Text>
+      <Text style={{ marginBottom: 20 }}>Si da click en el apartado de "menú" podra acceder a las pestañas.</Text>
     </View>
   );
 }
